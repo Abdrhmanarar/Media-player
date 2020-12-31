cc26b453d90346ad7269fb93f4e9206ee8388eb0
Abdrhmanarar <abdrhmanarar@icloud.com>
2020-12-31 07:43:38 +0200

    Add files via upload

---
+++ Media Player UI/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Media_Player_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CustomizDesign();
+        }
+        private void CustomizDesign()
+        {
+            panelMediaSubMenu.Visible = false;
+            panelPlaylistSubMenu.Visible = false;
+            PanelToolsSubMenu.Visible = false;
+            //..
+        }
+        private void hideSubMenu()
+        {
+            if (panelMediaSubMenu.Visible == true)
+                panelMediaSubMenu.Visible = false;
+            if (panelPlaylistSubMenu.Visible == true)
+                panelPlaylistSubMenu.Visible = false;
+            if (PanelToolsSubMenu.Visible == true)
+                PanelToolsSubMenu.Visible = false;
+        }
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelMediaSubMenu);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Form2());
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void panelPlaylistSubMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void btnPlaylist_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelPlaylistSubMenu);
+        }
+         private void button8_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void btnEqu_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Form3());
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void btnTools_Click(object sender, EventArgs e)
+        {
+            showSubMenu(PanelToolsSubMenu);
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            //..
+            //Your Code
+            //..
+            hideSubMenu();
+        }
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Media Player UI/Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Media_Player_UI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.button4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button4.Location = new System.Drawing.Point(12, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.button3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button3.Location = new System.Drawing.Point(428, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.Location = new System.Drawing.Point(428, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(428, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(63, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(307, 235);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(668, 403);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
